d3c0c17deba813986fa8c69e8570bd4a29334935
JamesBond <introbond.dev@gmail.com>
2023-04-06 09:45:49 +0700

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
-        <div className="App">
+        <div className="app">
             <h1>Vite + React</h1>
         </div>
     )
